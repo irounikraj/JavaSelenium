4ab317b0f6d44ed9328ac1e71cdede8f0f38a8ad
kumud <kumud@192.168.1.39>
2023-05-21 07:09:17 +0530

    Second Commit

---
+++ LearingJava/src/Lava/reverseString.java
@@ -0,0 +1,23 @@
+package Lava;
+import java.util.Scanner;
+
+public class reverseString {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner =new Scanner(System.in);
+		System.out.println("enter the string:");
+		String st= scanner.nextLine();
+		int n = st.length();
+		
+		System.out.println("the reversed string is");
+		
+		for (int i=n-1;i>=0;i--)
+		{
+			System.out.println(st.charAt(i));
+		}
+		
+
+	}
+
+}
